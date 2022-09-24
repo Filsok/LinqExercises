25b219c59979333d7a9889336e20353af75c42c9
Filip Sokolowski <filippawelsokolowski@gmail.com>
2022-09-24 11:52:41 +0200

    DataIntegration project created

---
+++ LinqDataIntegrationExercises/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDataIntegrationExercises
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+
+    }
+}
---
+++ LinqDataIntegrationExercises/Program.cs
@@ -0,0 +1,32 @@
+﻿using LinqDataIntegrationExercises;
+using Newtonsoft.Json;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var people = LoadPeople();
+        var addressess = LoadAddressess();
+    }
+
+    private static object LoadAddressess()
+    {
+        var currentDir = Directory.GetCurrentDirectory();
+        var jsonFullPath = Path.GetRelativePath(currentDir, "Person/Addressess.json");
+
+        var json = File.ReadAllText(jsonFullPath);
+
+        return JsonConvert.DeserializeObject<List<Person>>(json);
+    }
+
+    private static object LoadPeople()
+    {
+        var currentDir = Directory.GetCurrentDirectory();
+        var jsonFullPath = Path.GetRelativePath(currentDir, "Person/People.json");
+
+        var json = File.ReadAllText(jsonFullPath);
+
+        return JsonConvert.DeserializeObject<List<Person>>(json);
+    }
+}
